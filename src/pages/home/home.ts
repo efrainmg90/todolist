5a4d5c6d23525af14c173370fbdbb30f058f06aa
Efrain <efeainmg90@gmail.com>
2018-01-20 11:38:30 -0600

    initial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, reorderArray } from 'ionic-angular';
+import { TodoProvider } from '../../providers/todo/todo';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public todos = [];
+  public reorderIsEnabled = false;
+  constructor(private todoService: TodoProvider, public navCtrl: NavController, private alertCtrl: AlertController) {
+    this.todos = this.todoService.getTodos();
+  }
 
-  constructor(public navCtrl: NavController) {
+  toggleReorder(){
+    this.reorderIsEnabled = !this.reorderIsEnabled;
+  }
 
+  itemReordered($event){
+    reorderArray(this.todos,$event);
+  }
+  openTodoAlert(){
+    let addTodoAlert =this.alertCtrl.create({
+      title: "Add a todo",
+      message: "Enter your Todo",
+      inputs: [
+        {
+        type: "text",
+        name: "addTodoInput"
+      }
+    ],
+      buttons: [
+        {
+        text: "Cancel"
+      },
+      {
+        text: "Add Todo",
+        handler: (inputData)=>{
+          this.todoService.addTodo(inputData.addTodoInput);
+        }
+      }
+    ]
+    });
+    addTodoAlert.present();
   }
 
 }
